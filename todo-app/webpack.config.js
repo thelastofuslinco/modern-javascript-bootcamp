7df766861116671f2e36dd6c4649512b84e049b8
lincoln Duarte <lincolnskyrim_99@hotmail.com>
2023-10-02 14:24:26 -0300

    chore: add css minimizer

--- todo-app/webpack.config.js
+++ todo-app/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 
 module.exports = {
   entry: {
@@ -32,6 +33,7 @@ module.exports = {
     new MiniCssExtractPlugin()
   ],
   optimization: {
+    minimizer: [new CssMinimizerPlugin()],
     runtimeChunk: 'single'
   },
   module: {
